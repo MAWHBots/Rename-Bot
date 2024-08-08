da43e13a75e8242ab988ecd5f445469385987617
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-08-08 10:23:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,12 +10,12 @@ from helper.database import  insert ,find_one,used_limit,usertype,uploadlimit,ad
 from pyrogram.file_id import FileId
 from helper.database import daily as daily_
 from helper.date import add_date ,check_expi
-CHANNEL = os.environ.get('CHANNEL',"")
+CHANNEL = os.environ.get('CHANNEL',"-1001522888236")
 import datetime
 from datetime import date as date_
 STRING = os.environ.get("STRING","")
-log_channel = int(os.environ.get("LOG_CHANNEL",""))
-token = os.environ.get('TOKEN','')
+log_channel = int(os.environ.get("LOG_CHANNEL","-1001921484915"))
+token = os.environ.get('TOKEN','7239366223:AAHXc0KGwVCKnzeEAgX_G0FrUCSRpYUAyEQ')
 botid = token.split(':')[0]
 
 #Part of Day --------------------
